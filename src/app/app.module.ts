dd293ad70a1a3d1ac2e6da616ffc8f3d0c694f6b
Abdisamed <therealaa86@gmail.com>
2020-04-01 01:55:49 -0400

    test project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { MainSliderComponent } from './main-slider/main-slider.component';
+import { BookingComponent } from './booking/booking.component';
+import { AboutComponent } from './about/about.component';
+import { RoomsComponent } from './rooms/rooms.component';
+import { FeaturesComponent } from './features/features.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
+import { BlogComponent } from './blog/blog.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    MainSliderComponent,
+    BookingComponent,
+    AboutComponent,
+    RoomsComponent,
+    FeaturesComponent,
+    TestimonialComponent,
+    BlogComponent,
+    GalleryComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
